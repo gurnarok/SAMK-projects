b94d5ee5a1b5744508e788cba941158dd33d7485
Hannu Henttinen <hannu@ilari.fi>
2018-09-30 11:44:48 +0300

    Descriptions and optimization
    Added description and some equations.

---
+++ statics/README.md
@@ -0,0 +1,12 @@
+# Statics calculations
+
+##### statics_min_angle_force.py
+A homework I had to do, was to calculate the minimum force for F<sub>2</sub>, with range of angles from 0&deg; to 90&deg;.
+
+![Descriptive image](statics_min_angle_force.jpg)
+
+The beam implies a force of 6kN downwards in -Y direction.
+
+Simplified and doing all the required magic vector calculations, we get a formula of
+
+![Force equation](force_equation.svg)
--- statics/statics_min_angle_force.py
+++ statics/statics_min_angle_force.py
@@ -6,25 +6,34 @@ def force(angle,weight):
     force=weight/(1.74*np.sin(angle)+np.cos(angle))
 
     return force
-
+#END force
 def looper(angle_start, angle_end, decimals,n_decimals, weight):
-    old_force = 99999999999
-    old_angle = 99999999999
-
-    angle_start=angle_start*np.power(10,decimals)
-    angle_end=angle_end*np.power(10,decimals)
-    
-    for angle in range(angle_start, angle_end+1):
-        angle = angle/np.power(10,decimals)
-        new_force = force(angle,weight)
-        if old_force<=new_force:
+    for dec in range(0,decimals+1):
+        old_force = 99999999999
+        old_angle = 99999999999
+        angle_start = int(angle_start*(10**dec))
+        angle_end   = int(angle_end*(10**dec))
+        for angle in range(angle_start, angle_end+1):
+            angle = angle*(10**-dec)
+            new_force = force(angle,weight)
+            if old_force<=new_force:
                 print("Angle", end=' ')
-                print(round(angle,decimals), end=', ')
-                print("with force of", end=' ')
+                print(round(angle,dec))
+                print("Force", end=' ')
                 print(round(old_force,n_decimals),end=' kN\n')
                 #print("Angle {:f} degrees, with force of {:.14f} newtons".format(round(old_angle,decimals),old_force))
                 break
-        old_force = new_force
-        old_angle = angle
-    
-looper(0,90,2,10,6)
+            old_force = new_force
+            old_angle = angle
+        angle_start = angle-(10**-dec)
+        angle_end   = angle
+        # FOR angle
+    #FOR dec
+#END looper
+
+aStart = 0
+aEnd = 90
+decimalPoints = 5
+newtonDecimals = 10
+weightkiloNewtons = 6
+looper(0,90,5,10,6)
