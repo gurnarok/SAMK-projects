aa42e78426973b4f66c7a97a71d2ba11cbc861c1
Hannu Henttinen <grim.gurnarok+github@gmail.com>
2019-04-08 20:52:53 +0300

    Edited euler.py
    Using Decimal module, I get far more accurate reading on the euler

--- euler.py
+++ euler.py
@@ -1,11 +1,12 @@
 import math as m
-
+import decimal as d
 # Generate Euler number by summing 1 over factorial of n.
 # More info @ https://en.wikipedia.org/wiki/E_(mathematical_constant)
 n = int(input("How many factorials? "))
 tSum = 0
 for i in range(n+1):
     f = m.factorial(i)
-    tSum = tSum + (1/f)
-    # Does not change after n=17. Need to test other options.
-    print(f'i = {i:3d}  :  sum={tSum:.100f}')
+    # Using Decimal, I can get much accurate result
+    tSum = d.Decimal(tSum) + d.Decimal((1/f))
+    d.getcontext().prec = 200
+    print(f'i = {i:3d}  :  sum={tSum:.200f}')
